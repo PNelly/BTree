72f185fa35207b9d89715d11d6009021569b03ea
Patrick Doudy <patrick.doudy@gmail.com>
2017-07-28 18:51:09 -0600

    Encoder test driver

---
+++ EncoderDriver.java
@@ -0,0 +1,63 @@
+
+
+import java.util.Random; // for testing
+
+public class EncoderDriver {
+
+    public static void main(String[] args){
+
+	int numChars = 31;
+	int numTests = 1000;
+	int numSuccess = 0;
+	Encoder encoder = new Encoder(numChars);
+
+	StringBuilder builder;
+	Random RNG = new Random();
+	int rand;
+	String testInput, testOutput;
+	Long testLong;
+
+	for(int i=0;i<numTests;i++){
+
+	    // construct random test string
+	    builder = new StringBuilder();
+
+	    for(int j=0;j<numChars;j++){
+		rand = RNG.nextInt(4);
+		switch(rand){
+		case 0:
+		    builder.append('a');
+		    break;
+		case 1:
+		    builder.append('t');
+		    break;
+		case 2:
+		    builder.append('c');
+		    break;
+		case 3:
+		    builder.append('g');
+		    break;
+		}
+	    }
+
+	    // encode, decode, test eq
+	    testInput = builder.toString();
+	    testLong = new Long(encoder.encode(testInput));
+	    testOutput = encoder.decode(testLong.longValue());
+
+	    System.out.println(testInput);
+	    System.out.println(Long.toBinaryString(testLong));
+	    System.out.println(testOutput);
+	    if(testInput.equals(testOutput)){
+		System.out.println("Test Success!\n");
+		numSuccess++;
+	    } else
+		System.out.println("TEST FAILED\n");
+	}
+
+	System.out.println(numSuccess+" of "+numTests+" Tests Successful");
+
+	return;
+    }
+
+}
