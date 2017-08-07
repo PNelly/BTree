9a9af3069940d238a84cb9db5b04b36b33baeee4
ignominousicon17 <30513988+ignominousicon17@users.noreply.github.com>
2017-08-06 21:04:23 -0600

    GeneBankSearch additional argument checks and basic search sequence

--- GeneBankSearch.java
+++ GeneBankSearch.java
@@ -1,9 +1,19 @@
 import com.sun.javaws.exceptions.InvalidArgumentException;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.security.InvalidParameterException;
 
+
+/**
+ * Driver Class for searching a Gene Bank generated BTree for
+ * a list of query sequences.
+ *
+ * @author Ben Etcheverry
+ * CS321 Summer 2017
+ */
 public class GeneBankSearch {
 
     private static File BTreeFile;
@@ -11,6 +21,7 @@ public class GeneBankSearch {
     private static int cache;
     private static int cacheSize = 0;
     private static int debugLevel;
+    private static BTree tree;
     private static Encoder encoder;
 
     public static void main(String[] args) throws InvalidParameterException{
@@ -29,15 +40,32 @@ public class GeneBankSearch {
                 cache = Integer.parseInt(args[0]);
             }
             if(args.length > 3) {
-                if (Integer.parseInt(args[3]) < 1) {
-                    System.out.println("Cache size must be greater than 0.");
-                    printUsage();
-                    throw new InvalidParameterException();
-                } else {
-                    cacheSize = Integer.parseInt(args[3]);
+                if(Integer.parseInt(args[0]) == 1) {
+                    if (Integer.parseInt(args[3]) < 1) {
+                        System.out.println("Cache size must be greater than 0.");
+                        printUsage();
+                        throw new InvalidParameterException();
+                    } else {
+                        cacheSize = Integer.parseInt(args[3]);
+                    }
+                }
+                else{
+                    if(Integer.parseInt(args[4])!=0 && Integer.parseInt(args[4])!=1){
+                        System.out.println("Input argument <debug level> could not be parsed as 0 or 1.");
+                        printUsage();
+                        throw new InvalidParameterException();
+                    }
+                    else{
+                        debugLevel = Integer.parseInt(args[4]);
+                    }
                 }
                 if(args.length > 4){
-                    if(Integer.parseInt(args[4])!=0 && Integer.parseInt(args[4])!=1)){
+                    if(Integer.parseInt(args[0]) == 0){
+                        System.out.println("Cannot pass cache size when cache setting argument is set to 0");
+                        printUsage();
+                        throw new InvalidParameterException();
+                    }
+                    if(Integer.parseInt(args[4])!=0 && Integer.parseInt(args[4])!=1){
                         System.out.println("Input argument <debug level> could not be parsed as 0 or 1.");
                         printUsage();
                         throw new InvalidParameterException();
@@ -63,6 +91,29 @@ public class GeneBankSearch {
         }
 
 
+
+        try {
+            tree = new BTree(args[1]);
+            queryFile = new File(args[2]);
+            BufferedReader br = new BufferedReader(new FileReader(queryFile));
+            encoder = new Encoder(32);
+
+
+            String line = br.readLine();
+            if(tree.find(encoder.encode(line))!= null);{
+                System.out.println(line + " was found");
+            }
+            while(line != null){
+                line = br.readLine();
+                if(tree.find(encoder.encode(line))!= null);{
+                    System.out.println(line + " was found");
+                }
+            }
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     private static void printUsage(){
