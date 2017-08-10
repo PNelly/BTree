c8284a29e2a990d7e603697dc3636af36e765d89
KoenigKrote <Koenigkrote@outlook.com>
2017-08-09 20:02:02 -0600

    Fix equals check bug.  Remove unused NodeStorage methods.

--- NodeStorage.java
+++ NodeStorage.java
@@ -340,16 +340,4 @@ public final class NodeStorage {
         intBuf.get(array);
         return array;
     }
-
-    private static byte[] serialize(Object obj) throws IOException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ObjectOutputStream os = new ObjectOutputStream(out);
-        os.writeObject(obj);
-        return out.toByteArray();
-    }
-    private static Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
-        ObjectInputStream is = new ObjectInputStream(in);
-        return is.readObject();
-    }
 }
